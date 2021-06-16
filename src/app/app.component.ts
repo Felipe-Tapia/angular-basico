c40b85da76d9506193d0e9c5d5456e3fc8e0f4f6
Felipe Tapia <felipetapiade@gmail.com>
2021-06-16 15:15:02 -0400

    primer comentario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
 }
+
+
